8262f0f8ce7bbdb5e54bcf8d8b3b316f5015babc
42-Nexus <nexus20091@gmail.com>
2023-12-27 05:49:49 -0700

    init: 初始化项目结构和配置文件

---
+++ nexuschat-chat-server/src/main/java/com/venture/nexuschat/common/NexusChatCustomApplication.java
@@ -0,0 +1,7 @@
+package com.venture.nexuschat.common;
+
+public class NexusChatCustomApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ nexuschat-chat-server/src/main/java/com/venture/nexuschat/common/websocket/NettyWebSocketServerHandler.java
@@ -0,0 +1,23 @@
+package com.venture.nexuschat.common.websocket;
+
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.extra.spring.SpringUtil;
+import cn.hutool.json.JSONUtil;
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+@Sharable
+public class NettyWebSocketServerHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, TextWebSocketFrame textWebSocketFrame) throws Exception {
+
+    }
+}
